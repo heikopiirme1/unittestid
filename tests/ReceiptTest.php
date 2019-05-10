021bebcb9aae0f7b56601004ee4543f7f24b0188
Heiko <heiko.piirme@khk.ee>
2019-05-10 15:49:32 +0300

    03 Write a TDD method

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -11,13 +11,24 @@ class ReceiptTest extends TestCase {
     public function tearDown() {
         unset($this->Receipt);
     }
-    
+
     public function testTotal() {
         $Receipt = new Receipt(); // Luuakse uus tšekk
         $this->assertEquals( // Võrdleme, kas väärtused on võrdsed
             14, // Tulemus, mis peaks järgmiselt realt tulema
             $Receipt->total([0,2,5,8]), // Kasutatakse Receipt.php funktsiooni total, mis liidab arrays olevad liikmed kokku- antud juhul peaks vastus tulema 15
             'When summing the total should equal 15' // Kui vasted pole võrdsed, väljastatakse teade.
+        );
+    }
+
+    public function testTax(){
+        $inputAmount = 10.00; // Luuakse testimiseks parameetrid
+        $taxInput = 0.10;
+        $output = $this->Receipt->tax($inputAmount, $taxInput); // Kasutatakse funktsiooni tax, mis korrutab antud juhul inputAmount ja taxInput, vastus tuleb 1.0
+        $this->assertEquals( // Võrdleme, kas väärtused on võrdsed
+            1.0, // Oodatud väärtus
+            $output, // funktsiooni väärtus
+            'The tax calculation should equal 1.00' //Veateadet hetkel ei väljastata kuna vastus on õige
         );
     }
 }
